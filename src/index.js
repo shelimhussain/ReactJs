ac54a5e231fa91b35e2a80ed784f53c5df4dfbe7
Md. Shelim Ekbal Hussain <salim436@gmail.com>
2021-01-12 18:20:57 +0530

    update update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-/***************Let and const in ES5 and ES6 begin***************/
-
-/***************Let and const in ES5 and ES6 end***************/
-
-/***************Classes in ES5 and ES6 begin**************/
 import "./styles.css";
-var Es5class = function () {
-  this.name = "ES5";
-
-  this.sayes5Hello = function () {
-    return "Hi, I'm Learning " + this.name;
-  };
-};
-var instanceEs5 = new Es5class();
+/***************Let and const in ES5 and ES6 begin***************/
 class FirstClass {
   constructor() {
     this.name = "ES6";
   }
   sayHello() {
-    return "Hi, I'm going to learn " + this.name;
+    const times = 3;
+    const hiWord = "Hey ";
+    let hiPhrase = "";
+    for (let i = 0; i < times; i++) {
+      hiPhrase += hiWord;
+    }
+    return hiPhrase + ", I'm going to learn " + this.name;
+    /* We will use const before varibale when the value 
+    will not getchnaged till the scope of the varibale finished 
+    else we will use let keyword.*/
   }
 }
 var instance = new FirstClass();
-document.getElementById("app").innerHTML =
-  instanceEs5.sayes5Hello() + "<br><br>" + instance.sayHello();
-/***************Classes in ES5 and ES6 end**************/
+document.getElementById("app").innerHTML = instance.sayHello();
+/***************Let and const in ES5 and ES6 end***************/
