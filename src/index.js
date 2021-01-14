8a070ae6d8bad6b37508bb7c69211d619282f4c1
Md. Shelim Ekbal Hussain <salim436@gmail.com>
2021-01-14 17:06:35 +0530

    update update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-/*Modules coming soon*/
+import "./styles.css";
+import BaseClass, { allhiWords } from "./utiles";
+/***************Modules***************/
+class FirstClass extends BaseClass {
+  constructor() {
+    super(); /*to access properties of BaseClass*/
+    this._name = "ES6";
+  }
+
+  /*Getter method*/
+  get name() {
+    return `tech ${this._name}`;
+  }
+
+  /*Setter Method*/
+  set name(value) {
+    this._name = value;
+  }
+
+  sayHelloEs6() {
+    /*allhiWords varibale imported from utiles.js*/
+    let hiWords = allhiWords
+      .filter((x) => x.indexOf("He") === 0)
+      .map((x) => x.replace(" ", "o"));
+
+    let hiPhrase = "";
+
+    for (let hiWord of hiWords) {
+      hiPhrase += hiWord;
+    }
+
+    /* Arrow function*/
+    let great = () => {
+      return `${hiPhrase} , 
+      I'm going to learn ${this.name} 
+      by our teacher ${this.teacherName}`;
+    };
+
+    /* Here ${this.teacherName} is the property of BaseClass*/
+
+    return great();
+  }
+}
+var instance = new FirstClass();
+
+instance.name = "PHP"; /*Using setter method*/
+
+document.getElementById("app").innerHTML = instance.sayHelloEs6();
+/***************Modules***************/
---
+++ src/utiles.js
@@ -0,0 +1,9 @@
+class BaseClass {
+  constructor() {
+    this.teacherName = "Shelim";
+  }
+}
+
+export const allhiWords = ["Hey ", "Hi ", "Hello "];
+
+export default BaseClass;
